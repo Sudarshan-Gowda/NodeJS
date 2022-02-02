0741ce3e124584a917415d65f3ed76434165f987
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-02-02 22:51:58 +0530

    NJ-6: Dynamic routes

---
+++ 06-dynamic-routes/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const express = require("express");
+
+const shopController = require("../controllers/shop");
+const { route } = require("./admin");
+
+const router = express.Router();
+
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
