764a5821caf1767e0173382c57b6e83bd9ef4e3f
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-02-04 23:44:26 +0530

    adding files

--- 06-dynamic-routes/models/cart.js
+++ 06-dynamic-routes/models/cart.js
@@ -14,11 +14,17 @@ module.exports = class Cart {
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      const existingProduct = cart.products.find((prod) => prod.id === id);
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+
+      let existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
