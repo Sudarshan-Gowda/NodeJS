c36409e2afc72fa7a8e5427674d22ab5d6238c6b
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-02-10 11:18:52 +0530

    File changes

--- 07-mysql/app.js
+++ 07-mysql/app.js
@@ -17,6 +17,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-items");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-items");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -41,6 +43,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
@@ -55,12 +60,8 @@ sequelize
     return user;
   })
   .then((user) => {
-    //return user.createCart();
-    const cart = user.getCart();
-    if (!cart) {
-      return user.createCart();
-    }
-    return cart;
+    // console.log(user);
+    return user.createCart();
   })
   .then((cart) => {
     app.listen(3000);
